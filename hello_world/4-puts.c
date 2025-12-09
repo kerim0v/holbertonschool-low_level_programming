a70c67f039c89dd58cbde484444ffea25baf9605
kerim0v <kerimvtamerlan@gmail.com>
2025-12-09 13:35:26 +0000

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,14 @@
-/**
- * File: 4-puts.c
- */
-
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Description: prints string
- * \"Programming is like building a multilingual puzzle
- * Return: 0 Always
+ * Return: Always 0.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
-
