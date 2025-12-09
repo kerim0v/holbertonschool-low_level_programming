7a9354f22f37dfa039b8baf900ffa37fe4a1cfbb
kerim0v <kerimvtamerlan@gmail.com>
2025-12-09 13:26:01 +0000

    fix 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,10 +7,11 @@
 /**
  * main - this function prints out certain message in the standard output
  *
- * Return: 0
+ * Return: zero if code runes successfully
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
