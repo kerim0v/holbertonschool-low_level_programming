0e34478f4f842b2b9b815069ac2af00f94b5ee64
kerim0v <kerimvtamerlan@gmail.com>
2025-12-11 14:09:38 +0000

    s

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints th alphabt
+ *
+ * Return: Always 0
+ *
+ */
+
+int main (void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		if (l != 'q' && l != 'e')
+			putchar(l);
+	putchar('\n');
+
+	return (0);
+}
