ef46e25959ab7ae84d310548d9b1cfbf91ae1bff
kerim0v <kerimvtamerlan@gmail.com>
2025-12-13 21:21:37 +0000

    a

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * print_square - pdfp
- * @size: oidwjff
- * Return: safmf
+ * print_square - a function that prints a square, followed by a new line
+ * @size: size of both width and length
+ * Return: a square made of '#'
  */
 
 void print_square(int size)
 {
-	int i;
-	int n;
+	int co, ro;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (i = 1; i <= size; i++)
+		for (co = 1; co <= size; co++)
 		{
 			_putchar('#');
-
-			for (n = 2; n <= size; n++)
+			for (ro = 2; ro <= size; ro++)
 			{
-			       _putchar('#');
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
