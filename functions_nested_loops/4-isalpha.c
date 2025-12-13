9fea043bf70ceaa6ccbe3ae29de480df327a6da5
kerimv <kerimvtamerlan@gmail.com>
2025-12-13 18:50:33 +0000

    a

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - wdpfjpfi
+ * @c - mi bombo
+ * Return: 1 if success, 0 if no
+ */
+
+int _isalpha(int c)
+{
+	if ( c <= 'z' && c >= 'a' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
