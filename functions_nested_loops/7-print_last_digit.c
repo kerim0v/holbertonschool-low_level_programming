e5d98c7ea99b5f2d57bde556e0843c4305ce8232
kerimv <kerimvtamerlan@gmail.com>
2025-12-13 19:18:17 +0000

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,12 @@
 
 int print_last_digit(int c)
 {
-	int result = c % 10;
+	int result;
+       	result = (c % 10);
+
+	if (result < 0)
+		result = (-1 * result);
+
 	_putchar(result + '0');
 	return (result);
 }
