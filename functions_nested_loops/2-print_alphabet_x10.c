acd2c955407519d312d48cf927d2cca67119228e
kerimv <kerimvtamerlan@gmail.com>
2025-12-13 18:33:41 +0000

    a

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times richer
+ *
+ * Return: no
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n <= 10;)
+	{
+		for (i = 'a'; a <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+		n++;
+	}
+}
