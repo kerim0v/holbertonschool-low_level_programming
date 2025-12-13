e3ab9492c255e409e5a7581f4d6822718936f026
kerimv <kerimvtamerlan@gmail.com>
2025-12-13 20:19:42 +0000

    a

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - pasofkp
+ * @c: dsfj
+ * Return: 1 fidf, 0 odfks
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
