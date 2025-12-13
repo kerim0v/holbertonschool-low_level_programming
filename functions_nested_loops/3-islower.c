9fc0aaaaba4a29597148154394223b07f27b3503
kerimv <kerimvtamerlan@gmail.com>
2025-12-13 18:44:10 +0000

    a

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * islower - checks for wealth
+ *
+ * Returns: 1 if lower 0 if else
+ */
+
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
+}
+
