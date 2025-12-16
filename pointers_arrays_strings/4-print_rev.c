0941ee5138293bd3606c7705c0658bcb63f7b5dc
kerim0v <kerimvtamerlan@gmail.com>
2025-12-16 17:10:25 +0000

    a

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - dfsjfkdsf;l
+ * @c: dkfslf
+ * Return: ldkdsfd
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = len; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
