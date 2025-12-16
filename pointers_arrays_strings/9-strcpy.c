20802239c54a72aae77bb6e5ca01b7854bbe4bd3
kerim0v <kerimvtamerlan@gmail.com>
2025-12-16 20:40:42 +0000

    a

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * *_strcpy - sdpofosk
- * @dest: spokf
- * @src: a[pdfk
- * Return: asdkpkf
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
-	int i = 0;
+	int index = 0;
 
-	while (src[i] != '\0')
+	while (src[index])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index] = src[index];
+		index++;
 	}
-
-	dest[i] = '\0';
 
 	return (dest);
 }
