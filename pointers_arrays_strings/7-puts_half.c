e568a7e064a5d7ecaf84c856ad600099b1511f16
kerim0v <kerimvtamerlan@gmail.com>
2025-12-16 18:46:34 +0000

    a

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,26 +10,15 @@ void puts_half(char *str)
 {
 	int index = 0, length = 0;
 
-	while (str[index] != '\0')
+	while (str[length] != '\0')
 		length++;
 
-	if (length % 2 == 0)
+	index = (length + 1) / 2;
+
+	while (str[index] != '\0')
 	{
-		(length / 2) + 1; 
-		while (str[length] != '\0')
-		{
-			_putchar(str[length]);
-			length++;
-		}
-	}
-	else
-	{
-		(length + 1) / 2;
-		while (str[length] != '\0')
-		{
-		       	_putchar(str[length]);
-		       	length++;
-		}
+		_putchar(str[index]);
+		index++;
 	}
 	_putchar('\n');
 }
