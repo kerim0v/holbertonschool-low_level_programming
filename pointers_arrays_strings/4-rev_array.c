9979f527216eefe0982160b43b23da8efeb64c67
kerim0v <kerimvtamerlan@gmail.com>
2025-12-18 14:15:25 +0000

    a

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array(int *a, int n);
+ * @a: lkdjf
+ * @n: asdflkm
+ * Return: d;slgk
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+		i++;
+	}
+}
