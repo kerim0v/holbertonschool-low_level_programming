4d873284a9fc6b9e01aef09bfce59c737a870ab9
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 13:38:10 +0000

    a

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,19 +11,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 	int i;
-    
-    while (*s)
-    {
-	    for (i = 0; accept[i] != '\0'; i++)
-	    {
-		if (*s == accept[i])
-		    break;
-	    }
 
-	if (accept[i] == '\0')
-		return count;
-	count++;
-	s++;
-    }
-    return (count);
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				break;
+		}
+
+		if (accept[i] == '\0')
+			return count;
+		count++;
+		s++;
+	}
+	return (count);
 }
