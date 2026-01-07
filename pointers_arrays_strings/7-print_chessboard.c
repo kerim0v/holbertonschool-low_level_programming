a60ef1be94016bc9bf44429c23df1eb01c96d7fe
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 14:26:30 +0000

    a

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -14,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]);
-		putchar('\0');
+			_putchar(a[i][j]);
+		_putchar('\0');
 	}
 }
