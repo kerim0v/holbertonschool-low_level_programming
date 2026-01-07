646edcc49ac2d33e41235fb2b5ba6f5d5ce83782
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 13:22:17 +0000

    a

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-
-	for (; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (s);
+		if (*s == c)
+			return s;
 		s++;
 	}
+
+	if (*s == c)
+		return s;
+
 	return ('\0');	
 }
