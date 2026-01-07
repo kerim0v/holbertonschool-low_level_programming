e6ac3acf534b453a9eed888e622b2c409fe42487
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 13:52:20 +0000

    a

---
+++ pointers_arrays_strings/4-strbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strbrk - dogji
+ * @s: sdlkj
+ * @accept: ;dsakf
+ * Return: sdlkf
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++
+	}
+	return ('\0');
+}
