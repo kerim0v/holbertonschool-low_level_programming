18a88daf732e035f39132456eadfead2ce96a128
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 14:08:18 +0000

    a

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - pdgok
+ * @haystack: ;fsdlkg
+ * @needle: lsakf
+ * Return: a pointer to the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	while (*haystack != '\0')
+	{
+		i = 0;
+
+		while (neeedle[i] != '\0' && haystack[i] == needle [i])
+			i++;
+
+		if (needle[i] == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+	return (NULL);
+}
