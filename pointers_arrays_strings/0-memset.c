63d87d6fd96251e48654cb7873dc1b85a043a9ee
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 12:15:39 +0000

    a

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - efflkn
+ * @s: dfss
+ * @b: fsf
+ * @n: wpdfo
+ * Return: sfl
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
