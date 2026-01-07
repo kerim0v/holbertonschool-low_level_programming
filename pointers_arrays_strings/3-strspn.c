753362194f141c29a2de8d6b636401d80654e656
kerim0v <kerimvtamerlan@gmail.com>
2026-01-07 13:36:27 +0000

    a

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - ldkg
+ * @s: skg
+ * @accept: kslvk
+ * Return: dsokg
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i;
+    
+    while (*s)
+    {
+	    for (i = 0; accept[i] != '\0'; i++)
+	    {
+		if (*s == accept[i])
+		    break;
+	    }
+
+	if (accept[i] == '\0')
+		return count;
+	count++;
+	s++;
+    }
+    return (count);
+}
