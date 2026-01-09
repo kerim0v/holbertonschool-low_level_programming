f0d3041d2ef02ec783e355fce95536aacc5d73ce
kerim0v <kerimvtamerlan@gmail.com>
2026-01-09 12:55:16 +0000

    a

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - kdsk
+ * @n: ksdf
+ * Return: sdkjf
+ */
+
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+	else
+	{
+		factorial(n) = n * factorial(n - 1);
+		return (n);
+	}
+}
--- recursion/main.h
+++ recursion/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recurcion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 
 #endif
