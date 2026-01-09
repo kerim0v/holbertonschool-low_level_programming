ea05cc5bc6747348ccee6d64281df6167d730db1
kerim0v <kerimvtamerlan@gmail.com>
2026-01-09 12:12:55 +0000

    a

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - dsj
+ * @s: l;dsf
+ * Return: ksdljg
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		write(1, "\n", 1);
+		return;
+	}
+	write(1, s, 1);
+	_puts_recursion(s + 1);
+}
