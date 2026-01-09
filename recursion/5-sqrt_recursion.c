371a067f39a2164822d40101cd310c60b220946b
kerim0v <kerimvtamerlan@gmail.com>
2026-01-09 13:26:11 +0000

    a

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,29 +1,33 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - dsfkf
- * @n: dfs
- * Return: dfjd
+ * _sqrt_helper - finds the natural square root recursively
+ * @n: number to find the square root of
+ * @i: current guess
+ *
+ * Return: natural square root, or -1 if none exists
  */
-
-int _sqrt_recursion(int n)
-{
-	return (_sqrt_helper(n, 0));
-}
-
-/**
- * _sqrt_helper - dlfk
- * @n: dlfk
- * @i: dkgj
- * Return: dgjd
- */
-
 int _sqrt_helper(int n, int i)
 {
 	if (i * i == n)
 		return (i);
+
 	if (i * i > n)
 		return (-1);
 
 	return (_sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find the square root of
+ *
+ * Return: natural square root, or -1 if none exists
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt_helper(n, 0));
 }
