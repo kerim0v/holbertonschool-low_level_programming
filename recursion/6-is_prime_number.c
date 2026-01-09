f0ee3b040f5b8fdafdd925c0ff045ab4c21faae0
kerim0v <kerimvtamerlan@gmail.com>
2026-01-09 13:38:07 +0000

    a

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_prime_helper -dfj
+ * @n: df
+ * @i: skf
+ * Return: ds
+ */
+
+int is_prime_helper(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+
+	if (n % i == 0)
+		return(0);
+
+	return (is_prime_helper(n, i + 1));
+}
+
+
+/**
+ * is_prime_number - dslk
+ * @n: dlskf
+ * Return: kfjd
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_helper(n, 2));
+}
