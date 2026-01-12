194804b335e32ebe0a2871a37eaadec6534c889e
kerim0v <kerimvtamerlan@gmail.com>
2026-01-12 00:30:58 +0000

    a

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	char *endptr;
+	long num;
+	
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		num = strtol(argv[i], &endptr, 10);
+
+		if (*endptr != '\0' || num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		
+		sum += (int)num;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
