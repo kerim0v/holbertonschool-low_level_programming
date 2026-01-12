3085d07ca584416fd1b12573bb6603b1b48bb6a3
kerim0v <kerimvtamerlan@gmail.com>
2026-01-12 00:15:49 +0000

    a

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - dfd
+ * @argc: df
+ * @argv:sd
+ * Return: df
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	printf("%d\n", (argv[1] * argv[2]));
+	return (0);
+}
