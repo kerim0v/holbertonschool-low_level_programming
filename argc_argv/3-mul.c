e19d9d5aca7ecfbf7c6f8add9384f0ae86db9137
kerim0v <kerimvtamerlan@gmail.com>
2026-01-12 00:23:04 +0000

    a

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	a = strtoi(argv[1]);
-	b = strtoi(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
 	printf("%d\n", a * b);
 	return (0);
