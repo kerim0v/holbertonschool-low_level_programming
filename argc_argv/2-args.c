5be6c83b11dfaf49edbe47b3e0028365c4f60b38
kerim0v <kerimvtamerlan@gmail.com>
2026-01-12 00:10:11 +0000

    a

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - df
+ * @argc: sdkf
+ * @argv: kj
+ * Return: df
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 1; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
