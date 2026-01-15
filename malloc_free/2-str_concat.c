d6e5223b3464199d91f37636b6d99d105f34e5e3
kerim0v <kerimvtamerlan@gmail.com>
2026-01-15 10:16:21 +0000

    a

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -21,12 +21,12 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
-	
+
 	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
 
-	string = malloc ((len1 + len2 + 1) * sizeof(char));
+	string = malloc((len1 + len2 + 1) * sizeof(char));
 	if (string == NULL)
 		return (NULL);
 
