df02589bb3b83d3a6a5823cbebf28c618ccdb8f9
kerim0v <kerimvtamerlan@gmail.com>
2026-01-15 10:24:56 +0000

    a

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return NULL;
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return NULL;
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i > 0)
+			{
+				i--;
+				free(grid[i]);
+			}
+			free(grid);
+			return NULL;
+		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return grid;
+}
