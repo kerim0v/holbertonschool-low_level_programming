0a352649ecf35a99f7e34476a6dc4ae07c8a01dd
kerim0v <kerimvtamerlan@gmail.com>
2026-01-15 09:49:39 +0000

    a

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdlib.h"
+
+/** _strdup - DF
+ * @str: df
+ * Return: dg
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *string;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	string = malloc((len + 1) * sizeof(char));
+	if (string == NULL)
+		return (NULL);
+
+	for (; i <= len; i++)
+		string[i] = str[i];
+
+	return (string);
+}
