33fbfb9ab1fdbc610fa3d1d2916bf1b4bb75bdd7
kerim0v <kerimvtamerlan@gmail.com>
2026-01-15 09:25:02 +0000

    a

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * create_array - df
+ * @size: sdf
+ * @c: dsf
+ * Return: nullll
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc( size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+	return (array);
+}
