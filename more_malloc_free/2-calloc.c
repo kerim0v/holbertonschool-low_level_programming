0e540f3ed119c32d1585db7508b46dce906153f8
kerim0v <kerimvtamerlan@gmail.com>
2026-01-17 14:55:08 +0000

    a

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _calloc - dfdf
@@ -10,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
-	unsigned int;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
