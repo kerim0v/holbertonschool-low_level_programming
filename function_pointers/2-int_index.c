3269280c030b7eb739f09a8865adecfe1a8b1c57
kerim0v <kerimvtamerlan@gmail.com>
2026-01-26 15:19:26 +0000

    a

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "funstion_pointers.h"
+
+/**
+ * int_index - sad
+ * @array: asda
+ * @size: dasdsaf
+ * @cmp: asf
+ * Return: sad
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0)
+				return (i);
+	}
+
+	return (-1);
+}
