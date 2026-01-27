4e4e1079855dc882c5b0019c4e9cec1619b9cd5f
kerim0v <kerimvtamerlan@gmail.com>
2026-01-27 23:21:26 +0000

    a

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - sad
+ * Return: saasdfw
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, result;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(a, b);
+
+	printf("&d\n", result);
+
+	return (0);
+}
