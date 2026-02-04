b633b205cb09278d7d293625783d8ea26c65bc54
kerim0v <kerimvtamerlan@gmail.com>
2026-02-04 20:49:27 +0000

    a

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node - asdff
+ * @head: asdfs
+ * @str: a
+ * Return: safg
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = 0;
+	while (str[new_node->len])
+		new_node->len++;
+
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+		
+}
