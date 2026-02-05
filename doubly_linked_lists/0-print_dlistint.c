0768624da8d5f0b7ef9efc2738984373131df23b
kerim0v <kerimvtamerlan@gmail.com>
2026-02-05 23:13:43 +0000

    a

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_dlistint - asf
+ * @h: saffas
+ * Return: asfa
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
