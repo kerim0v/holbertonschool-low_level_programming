db90b8266843d97d074df945f07c46a9cf254d9a
kerim0v <kerimvtamerlan@gmail.com>
2026-02-05 23:34:11 +0000

    a

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 
-	if (*head == NULL)
+	if (*head != NULL)
 		(*head)->prev = new;
 
 	*head = new;
