bf69914ded67a6298ffdf245d951cd19b376abd4
kerim0v <kerimvtamerlan@gmail.com>
2026-02-07 23:15:11 +0000

    a

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - asfa
+ * @filename:csad
+ * letters: asda
+ * Return: asfa
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0)
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+
+	if (write == -1)
+	{
+		return (0);
+	}
+
+	close(fd);
+}
