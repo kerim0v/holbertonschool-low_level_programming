cead1b62fced1df6e7414af91c6ec2aea74c642d
kerim0v <kerimvtamerlan@gmail.com>
2026-02-09 14:18:02 +0000

    a

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,20 +9,42 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0)
-
+	int fd;
+	char *buf;
+	ssize_t r_bytes, w_bytes;
+	
 	if (filename == NULL)
 	{
 		return (0);
 	}
 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-	if (write == -1)
+	buf = malloc(sizeof(char) * letter);
+	if (buf == NULL)
 	{
+		close(fd);
 		return (0);
 	}
 
+	r_bytes = read(fd, buf, letters);
+	if (r_bytes == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	w_bytes = write(STDOUT_FILENO, buf, r_bytes);
+	if (w_bytes != r_bytes)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
 	close(fd);
 }
