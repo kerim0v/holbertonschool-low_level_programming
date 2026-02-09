73b9711d3bed475e18fa22d28e2dd679fe70e430
kerim0v <kerimvtamerlan@gmail.com>
2026-02-09 14:50:30 +0000

    a

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - asfa
+ * @filename: as
+ * @text_content: afaf
+ * Return: asdf
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t w_bytes;
+	ssize_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		w_bytes = write(fd, text_content, len);
+		if (w_bytes == -1 || (size_t)w_bytes != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
