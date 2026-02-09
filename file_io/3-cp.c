e1bd6edb59f64c3f3a9a0c789e392026a86e09cc
kerim0v <kerimvtamerlan@gmail.com>
2026-02-09 15:13:52 +0000

    a

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open(argv[1], 0_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
