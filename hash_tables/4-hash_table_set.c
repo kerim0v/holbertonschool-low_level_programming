0ef8a8418799beed46e461f9a028707c0fff1e2a
kerim0v <kerimvtamerlan@gmail.com>
2026-03-04 18:51:55 +0000

    a

---
+++ hash_tables/4-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - asdf
+ * @ht: asfg
+ * @key: asfa
+ * Return: asfaf
+ */
+
+char *hash_tables_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size_);
+
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+
+		node = node->next;
+	}
+
+
+	return (NULL);
+}
