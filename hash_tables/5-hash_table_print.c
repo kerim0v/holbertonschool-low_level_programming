283a1927b7222af2a226e18be639b5fe8256aa1a
kerim0v <kerimvtamerlan@gmail.com>
2026-03-12 14:11:39 +0000

    a

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,43 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_print - saff
+ * @ht: asf
+ * Return: asf
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (!ht)
+	{
+		return;
+	}
+
+	printg("{");
+	for (i = 0; i < ht->size; i)
+	{
+		node = ht->array[i];
+		while (!node)
+		{
+			printf("'%s' : '%s'", node->key, node->value);
+			node = node->next;
+			if (!node)
+			{
+				printf(", ");
+			}
+			else if (i < ht->size - 1)
+			{
+				unsigned long int j = i + 1;
+
+				while (j < ht->size && ht->array[j] == NULL)
+					j++;
+				if (j < ht->size)
+					printf(", ");
+			}
+		}
+	}
+	printf("}\n");
+}
